1339a35aa067c619513fae31b7eb63b4938c7b3b
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-04 10:33:18 +0200

    font-mixins added

--- src/components/logout/LogoutBtn.jsx
+++ src/components/logout/LogoutBtn.jsx
@@ -8,7 +8,9 @@ export default function LogoutBtn() {
 
     function handleLogout(){
         logout()
-        navigate("/")
+        setTimeout(() => {
+            navigate("/")
+        }, 0);
     }
 
     return token?(
